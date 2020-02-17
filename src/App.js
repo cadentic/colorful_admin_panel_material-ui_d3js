0cd873b63f5f5b602ed9fc2030a06d090191424f
AndrewFucher <matches708@gmail.com>
2020-02-17 20:39:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      background: '#ffffff',
+      color: '#606060',
+  },
+  list: {
+	width: 250,
+  },
+}));
 
 function App() {
+
+  const [state, setState] = React.useState({
+    isSideBar: false,
+  });
+
+  const classes = useStyles();
+  let sidebar = null;
+  if (state.isSideBar) {
+    	sidebar =
+        <div className={classes.list} onClick={() => setState({
+			...state,
+			isSideBar: !state.isSideBar,
+		})}>
+            <Drawer open={state.isSideBar}
+			>
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </div>;
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Button onClick={()=>setState({
+            ...state, 
+            isSideBar: !state.isSideBar,
+          })}>
+            <img alt="qwerty" src="https://unsplash.it/40/40"/>
+          </Button>
+          <Typography variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {sidebar}
+      
     </div>
   );
 }
